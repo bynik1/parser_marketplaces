96bccc60eba88d1474f9eaccb32dfc1540a881af
bynik <alesha.bessonov2003@mail.ru>
2025-05-13 13:56:58 +0700

    добавил авторизацию пользователей

--- parser_marketplaces/parser_marketplaces/urls.py
+++ parser_marketplaces/parser_marketplaces/urls.py
@@ -22,6 +22,7 @@ from main.views import page_not_found
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 handler404 = page_not_found
