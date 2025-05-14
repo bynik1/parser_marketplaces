b3c6219989ce8cb3a69f33e7e4a3c8bc34c064a7
bynik <alesha.bessonov2003@mail.ru>
2025-05-14 11:19:24 +0700

    улучшения формы регестрации

--- parser_marketplaces/users/views.py
+++ parser_marketplaces/users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from .forms import LoginUserForm, RegisterUserForm
 
@@ -13,14 +14,8 @@ class LoginUser(LoginView):
     template_name = 'users/login.html'
     extra_context = {'title': 'Авторизация'}
 
-def register(request):
-    if request.method == 'POST':
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return render(request, 'users/register_done.html')
-    else:
-        form = RegisterUserForm()
-    return render(request, 'users/register.html', {'form': form})
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': "Регистрация"}
+    success_url = reverse_lazy('users:login')
