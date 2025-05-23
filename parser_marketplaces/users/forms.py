9d59c9d827e8d62368b5887b39f894c663fd4c32
bynik <alesha.bessonov2003@mail.ru>
2025-05-23 19:43:11 +0700

    добавил добавления телеграмм юзернейма и номера телефона при регестарции и в профиле

--- parser_marketplaces/users/forms.py
+++ parser_marketplaces/users/forms.py
@@ -19,19 +19,24 @@ class RegisterUserForm(UserCreationForm):
     username = forms.CharField(label="Логин", widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-
+    phone = forms.CharField(label="Номер телефона", required=False, widget=forms.TextInput(attrs={'class': 'form-input'}))
+    telegram_username = forms.CharField(label="Telegram Username", required=False, widget=forms.TextInput(attrs={'class': 'form-input'}))
     class Meta:
         model = get_user_model()
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'phone', 'telegram_username', 'password1', 'password2']
         labels = {
             'email': 'E-mail',
             'first_name': "Имя",
             'last_name': "Фамилия",
+            'phone': 'Номер телефона',
+            'telegram_username': 'Telegram Username',
         }
         widgets = {
             'email': forms.TextInput(attrs={'class': 'form-input'}),
             'first_name': forms.TextInput(attrs={'class': 'form-input'}),
             'last_name': forms.TextInput(attrs={'class': 'form-input'}),
+            'phone': forms.TextInput(attrs={'class': 'form-input'}),
+            'telegram_username': forms.TextInput(attrs={'class': 'form-input'}),
         }
 
     def clean_email(self):
@@ -39,6 +44,18 @@ class RegisterUserForm(UserCreationForm):
         if get_user_model().objects.filter(email=email).exists():
             raise forms.ValidationError("Такой E-mail уже существует!")
         return email
+    
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if phone and get_user_model().objects.filter(phone=phone).exists():
+            raise forms.ValidationError("Такой номер телефона уже существует!")
+        return phone
+
+    def clean_telegram_username(self):
+        tg_username = self.cleaned_data.get('telegram_username')
+        if tg_username and get_user_model().objects.filter(telegram_username=tg_username).exists():
+            raise forms.ValidationError("Такой Telegram Username уже существует!")
+        return tg_username
 
 class CustomFileInput(forms.ClearableFileInput):
     template_name = 'users/widgets/custom_file_input.html'
@@ -51,15 +68,19 @@ class ProfileUserForm(forms.ModelForm):
 
     class Meta:
         model = get_user_model()
-        fields = ['photo', 'username', 'email', 'date_birth', 'first_name', 'last_name']
+        fields = ['photo', 'username', 'email', 'date_birth', 'first_name', 'last_name', 'phone', 'telegram_username']
         labels = {
             'first_name': 'Имя',
             'last_name': 'Фамилия',
+            'phone': 'Номер телефона',
+            'telegram_username': 'Telegram Username',
         }
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-input'}),
             'last_name': forms.TextInput(attrs={'class': 'form-input'}),
             'photo': CustomFileInput,
+            'phone': forms.TextInput(attrs={'class': 'form-input'}),
+            'telegram_username': forms.TextInput(attrs={'class': 'form-input'}),
         }
 
 
